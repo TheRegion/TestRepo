5389218738c029541810ab82e2d79c927721575c
Ali Shahrooz <ali@netmasons.com>
2016-07-07 15:08:08 -0400

    updates

---
+++ TestRepoConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test repo things are happening!");
+        }
+    }
+}
